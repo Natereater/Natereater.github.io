4a1844a913816615700d7806e04b3aa278b19f83
Natereater <eiseneater@gmail.com>
2024-07-16 20:57:18 -0700

    no results for us

--- jsonify_league_runs.py
+++ jsonify_league_runs.py
@@ -16,16 +16,17 @@ def jsonify_player_team_runs():
                  "scores": [],
                  "cumulative_score": [0]}
     
-    for i, row in df.iterrows():
-        this_mission = row.to_dict()
-        this_score = drg_scoring.get_mission_score(row)
-        this_mission["Score"] = this_score
-        final_json["missions"].append(this_mission)
+    if len(df) > 0:
+        for i, row in df.iterrows():
+            this_mission = row.to_dict()
+            this_score = drg_scoring.get_mission_score(row)
+            this_mission["Score"] = this_score
+            final_json["missions"].append(this_mission)
 
-        final_json["scores"].append(this_score)
-        current_cumulative_score = simulation_generator.get_cumulative_score(final_json["scores"])
+            final_json["scores"].append(this_score)
+            current_cumulative_score = simulation_generator.get_cumulative_score(final_json["scores"])
 
-        final_json["cumulative_score"].append(current_cumulative_score)
+            final_json["cumulative_score"].append(current_cumulative_score)
     
     with open('drg_assets/playable_team_results.json', 'w') as f:
             json.dump(final_json, f)
